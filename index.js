1847d9b18ffa24383f215281a5142306adf94b18
Kylir <Kylir@users.noreply.github.com>
2016-09-30 16:07:58 +0100

    Fix random colour generation.

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 /* jshint undef: true, node: true, eqeqeq: true, esnext: true */
 
+var NUMBER_OF_LEDS = 32;
 var strip = require('rpi-ws281x-native');
-strip.init(32);
+strip.init(NUMBER_OF_LEDS);
 strip.setBrightness(100);
 
 var leds = [
     0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0xff0000    
+    0,0,0,0,0,0,0,0    
 ];
 
+// Loop every 0.1 sec: randomise an index and a colour and display it
 strip.render(leds);
 var interval = setInterval(function() {
-    console.log('Rendering!');
-    var led = leds.shift();
-    leds.push(led);
+    var randomIndex = Math.floor(Math.random() * NUMBER_OF_LEDS);
+    var randomColour = Math.floor(Math.random() * 0xffffff);
+    leds[randomIndex] = randomColour;
+    console.log('Rendering colour ' + randomColour + ' at index ' + randomIndex);
     strip.render(leds);
 }, 100);
 
+// After 10 secondes, stop.
 setTimeout(function () {
     console.log('Stop!');
     clearInterval(interval);
