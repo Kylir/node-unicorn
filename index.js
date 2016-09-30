e51be5b9c910de4d669936d0b2cd71ca9eff80df
Kylir <Kylir@users.noreply.github.com>
2016-09-30 16:01:44 +0100

    New experiment. Random index and random colour.

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 /* jshint undef: true, node: true, eqeqeq: true, esnext: true */
 
 var strip = require('rpi-ws281x-native');
-
 strip.init(32);
 strip.setBrightness(100);
 
 var leds = [
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0xff0000    
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0xff0000    
 ];
 
+strip.render(leds);
 var interval = setInterval(function() {
-    strip.render(leds);
     console.log('Rendering!');
     var led = leds.shift();
     leds.push(led);
+    strip.render(leds);
 }, 100);
 
 setTimeout(function () {
