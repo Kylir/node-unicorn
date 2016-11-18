1510710fa8740e4ecef55a80c5db674688e241fc
Kylir <Kylir@users.noreply.github.com>
2016-11-18 18:39:39 +0000

    Beginning of a game server. No validation on the moves.

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -5,14 +5,47 @@
 
 var game = require('../lib/rubicorn/game');
 
-describe('The game module', function() {
+describe('In the game module', function() {
 
-    // We want to generate a random grid using 
+    // We want to generate a random grid
+    describe('The grid generator', function () {
 
-    describe('The color matrix', function () {
+        function isAValidColor (color) {
+            return ((color >= 0) && (color <= 0xffffff));
+        }
 
-        it('should have some red stuff', function() {
-            expect(game.colors.R).toEqual(0xff0000);
+        it('should generate valid grids.', function() {
+            let numCols = 8;
+            let numLines = 4;
+
+            //Do it 5 times...
+            for (let n=0; n<5; n++) {
+                let randomGrid = game.generateGrid(numLines, numCols);
+                expect(randomGrid.length).toEqual(numLines);
+                for (let i=0; i<numLines; i++) {
+                    expect(randomGrid[i].length).toEqual(numCols);
+                    for (let j=0; j<numCols; j++) {
+                        expect(isAValidColor(randomGrid[i][j])).toEqual(true);
+                    }
+                }
+            }
         });
     });
+
+    // We want a function to tell if we won
+    describe('The areIdentical() function', function () {
+        it('should recognise two identical matrices.', function () {
+            let m1 = [[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4]];
+            expect(game.areIdentical(m1, m1)).toEqual(true);
+        });
+
+        it('should not accept two different matrices.', function () {
+            let m1 = [[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4]];
+            let m2 = [[1,2,3,4,1,2,3,4],[0,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4],[1,2,3,4,1,2,3,4]];
+            //        difference here -> X
+            expect(game.areIdentical(m1, m2)).toEqual(false);
+        });
+    });
+    
+
 });
